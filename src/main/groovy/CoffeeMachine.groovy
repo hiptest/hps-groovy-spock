64a3d4fe992798bf90e0050c95cd2a7a45cafd73
Vincent Pretre <vincent.pretre@gmail.com>
2017-08-24 17:19:30 +0200

    Add descaling capabilities + cheat sheet for tutorials

--- src/main/groovy/CoffeeMachine.groovy
+++ src/main/groovy/CoffeeMachine.groovy
@@ -8,6 +8,7 @@ class CoffeeMachine {
   Integer tankContent;
   Integer beansContent;
   Integer groundsContent;
+  Integer countdownToDescale;
 
   String lang;
   String waterHardness = "2";
@@ -17,6 +18,7 @@ class CoffeeMachine {
     fillTank();
     fillBeans();
     emptyGrounds();
+    descale();
   }
 
   def start(lang = "en") {
@@ -40,6 +42,14 @@ class CoffeeMachine {
     groundsContent = 0;
   }
 
+  def descale() {
+    countdownToDescale = 500;
+  }
+
+  def isDescalingNeeded() {
+    return countdownToDescale <= 0;
+  }
+
   def showSettings() {
     settingsDisplayed = true;
   }
@@ -59,14 +69,16 @@ class CoffeeMachine {
         beans: 'Fill beans',
         grounds: "Empty grounds",
         ready: "Ready",
-        settings: "Settings:\n - 1: water hardness\n - 2: grinder"
+        settings: "Settings:\n - 1: water hardness\n - 2: grinder",
+        descale: "Descaling is needed"
       ],
       fr: [
         tank: 'Remplir reservoir',
         beans: 'Ajouter grains',
         grounds: "Vider marc",
         ready: "Pret",
-        settings: "Configurer:\n - 1: durete de l'eau\n - 2: mouture"
+        settings: "Configurer:\n - 1: durete de l'eau\n - 2: mouture",
+        descale: "Detartrage requis"
       ],
     ]
 
@@ -80,6 +92,7 @@ class CoffeeMachine {
     if (tankContent <= 10) return i18n("tank");
     if (beansContent < 3) return i18n("beans");
     if (groundsContent >= 30) return i18n("grounds");
+    if (isDescalingNeeded()) return i18n("descale");
     return i18n("ready");
   }
 
@@ -91,6 +104,7 @@ class CoffeeMachine {
       tankContent -= 1;
       beansContent -= 1;
       groundsContent += 1;
+      countdownToDescale -= 1;
     }
   }
 }
